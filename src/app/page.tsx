8f7ab062a83dd5e7cb6c963c77ff0bbd2f1f2229
valonmulolli <valonmulolli@gmail.com>
2024-01-18 22:29:47 +0100

    NavItem and NavItems components for improved structure and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
 				<div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
 					<h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
 						Your marketplace for high-quality{' '}
-						<span className='text-green-400'>digital assets</span>.
+						<span className='text-green-500'>digital assets</span>.
 					</h1>
 					<p className='mt-6 text-lg max-w-prose text-muted-foreground'>
 						Welcome to TechTrove. Every asset on our platform is verified by our
---
+++ src/products/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: 'Gpu',
+    value: 'Gpu' as const,
+    featured: [
+      {
+        name: 'Editor picks',
+        href: '#',
+        imageSrc: '/nav/gpu/4080super.jpg',
+      },
+      {
+        name: 'New Arrivals',
+        href: '#',
+        imageSrc: '/nav/gpu/409024G.jpg',
+      },
+      {
+        name: 'Bestsellers',
+        href: '#',
+        imageSrc: '/nav/gpu/RX7900XTX.jpg',
+      },
+    ],
+  },
+  {
+    label: 'keyboards',
+    value: 'keyboards' as const,
+    featured: [
+      {
+        name: 'Favorite keyboard Picks',
+        href: '#',
+        imageSrc: '/nav/keyboards/5.jpg',
+      },
+      {
+        name: 'New Arrivals',
+        href: '#',
+        imageSrc: '/nav/keyboards/6.jpg',
+      },
+      {
+        name: 'Bestselling keyboards',
+        href: '#',
+        imageSrc: '/nav/keyboards/7.jpg',
+      },
+    ],
+  },
+]
